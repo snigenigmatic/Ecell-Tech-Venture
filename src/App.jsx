c214bae28c4aab3630ef535a17e4398f2351d1bc
snigenigmatic <kaustubhc.11052gmail.com>
2024-04-24 10:11:22 +0530

    added a carouse(needs improvement)

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,19 @@ import Header from './assets/components/Header'
 import Hero from './assets/components/Hero'
 import Carousel from './assets/components/Carousel'
 
-function App() {
 
+function App() {
   return (
     <>
       <div className='App'>
         <Header/>
-        <div className='w-full h-screen'>
+        <div className='w-svh h-svh'>
           <Hero/>
         </div>
-        <div className='w-svw h-svh'>
-          {/* Carousel */}
-          
+        <div className='flex flex-row bg-gradient-to-tr from-blue-400 toblue-950'>
+          <div className='flex flex-row w-1/2 h-1/4 px-3 py-3'>
+            <Carousel/>
+          </div>
         </div>
       </div>
     </>
--- src/assets/components/Carousel.jsx
+++ src/assets/components/Carousel.jsx
@@ -2,32 +2,37 @@ import React from 'react'
 
 const Carousel = () => {
   return (
-    <div>
-      <CCarousel controls indicators>
-  <CCarouselItem>
-    <CImage className="d-block w-100" src={ReactImg} alt="slide 1" />
-    <CCarouselCaption className="d-none d-md-block">
-      <h5>First slide label</h5>
-      <p>Some representative placeholder content for the first slide.</p>
-    </CCarouselCaption>
-  </CCarouselItem>
-  <CCarouselItem>
-    <CImage className="d-block w-100" src={VueImg} alt="slide 2" />
-    <CCarouselCaption className="d-none d-md-block">
-      <h5>Second slide label</h5>
-      <p>Some representative placeholder content for the first slide.</p>
-    </CCarouselCaption>
-  </CCarouselItem>
-  <CCarouselItem>
-    <CImage className="d-block w-100" src={AngularImg} alt="slide 3" />
-    <CCarouselCaption className="d-none d-md-block">
-      <h5>Third slide label</h5>
-      <p>Some representative placeholder content for the first slide.</p>
-    </CCarouselCaption>
-  </CCarouselItem>
-</CCarousel>
+    <div className="carousel w-full">
+  <div id="slide1" className="carousel-item relative w-full">
+    <img src="https://daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg" className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide4" className="btn btn-circle">❮</a> 
+      <a href="#slide2" className="btn btn-circle">❯</a>
     </div>
+  </div> 
+  <div id="slide2" className="carousel-item relative w-full">
+    <img src="https://daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg" className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide1" className="btn btn-circle">❮</a> 
+      <a href="#slide3" className="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide3" className="carousel-item relative w-full">
+    <img src="https://daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg" className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide2" className="btn btn-circle">❮</a> 
+      <a href="#slide4" className="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide4" className="carousel-item relative w-full">
+    <img src="https://daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide3" className="btn btn-circle">❮</a> 
+      <a href="#slide1" className="btn btn-circle">❯</a>
+    </div>
+  </div>
+</div>
   )
 }
 
-export default Carousel;
+export default Carousel
