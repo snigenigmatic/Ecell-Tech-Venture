eab67d4fed90341b4192723be4c4f0938522f1cc
snigenigmatic <kaustubhc.11052gmail.com>
2024-09-07 14:34:14 +0530

    idk

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import './App.css'
 import Header from './assets/components/Header'
 import Hero from './assets/components/Hero'
-import Carousel from './assets/components/Carousel'
 
 
 function App() {
@@ -15,7 +14,6 @@ function App() {
         </div>
         <div className='flex flex-row bg-gradient-to-tr from-blue-400 toblue-950'>
           <div className='flex flex-row w-1/2 h-1/4 px-3 py-3'>
-            <Carousel/>
           </div>
         </div>
       </div>
